2487b8b4ccf0fc3c87e94409d8b892f406cf6875
Mike <Icemberth.Mike@gmail.com>
2019-01-27 17:12:16 -0500

    adding asset images for VP

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import List from './pages/List';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
+          <Route exact path='/' component={Home}/>          
         </Switch>
       </div>
     )
