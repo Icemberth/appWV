68c487fb4581db5c26e02b7326b0cc00aa452cb1
Mike <Icemberth.Mike@gmail.com>
2019-01-27 20:34:25 -0500

    adding node modules

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
+import './App.scss';
+import Home from './home/Home';
 
 class App extends Component {
   render() {
-    const App = () => (
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>          
-        </Switch>
-      </div>
-    )
     return (
       <Switch>
-        <App/>
+        <Route exact path='/' component={Home}/> 
       </Switch>
     );
   }
---
+++ client/src/App/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className="wrapper">
+        <div className="ribbon-wrapper-green">
+            <div className="ribbon-green">NEWS</div>
+        </div>
+    </div>
+    );
+  }
+}
+export default Home;
