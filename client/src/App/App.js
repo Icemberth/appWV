0c714acf0b098f64483262e695ea4ef65ca0bf75
jumcorrealo <jumcorrealo@unal.edu.co>
2019-01-31 20:31:46 -0500

    propuesta de diseño 1

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -6,11 +6,13 @@ import Home from './home/Home';
 class App extends Component {
   render() {
     return (
+      
       <div className="app">
         <Switch>
           <Route exact path='/' component={Home}/> 
         </Switch>
       </div>
+      
     );
   }
 }
--- client/src/App/home/Home.js
+++ client/src/App/home/Home.js
@@ -5,9 +5,32 @@ import './Home.scss';
 class Home extends Component {
   render() {
     return (
-      <div className="wrapper">        
-        Hola que hace, ji ji ji
+      
+      <div className="Hero">
+        <div className="wrapper">
+          Vampiro V5 
+          <p className="p1">Si un
+            personaje falla en una acción, a
+            veces puede volver a intentarla.
+            Después de todo, fallar al forzar
+            una cerradura no significa que el
+            personaje no pueda insertar una
+            ganzúa en esa puerta nunca más.
+            Las circunstancias deben justificar
+            un intento posterior (por ejemplo,
+            el personaje obtiene un juego de
+            ganzúas mejor o su habilidad se ha
+            incrementado desde la última vez).</p>
+        </div>      
+        <div className="Facebook">
+        Facebook
+        </div>
+        <div className="twitter">
+        Twitter
+
+        </div>
       </div>
+      
     );
   }
 }
