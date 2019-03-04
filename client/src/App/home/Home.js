ad1a9f81cd5d9e98af463a8dd890ac00ed5eefd7
Mike <Icemberth.Mike@gmail.com>
2019-03-04 08:50:48 -0500

    Revert "Sebs"
    This reverts commit fb26fd83b8e7a6fefe0e603ee37459f448184ea9.

--- client/src/App/home/Home.js
+++ client/src/App/home/Home.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 import './Home.scss';
 
-import Sebs from './../components/sebs/Sebs';
-
 class Home extends Component {
   render() {
     return (
       <div className="wrapper">        
-        <Sebs />
+      
       </div>
     );
   }
